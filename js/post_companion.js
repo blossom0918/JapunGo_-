ba9f3dbd8780091711a2730df78d990f58550279
KouShuei <m989699@gmail.com>
2019-12-08 18:42:49 +0800

    頭貼評論未完成

--- js/post_companion.js
+++ js/post_companion.js
@@ -1,7 +1,28 @@
 db = firebase.database();
 
+
+
 function more(key) {
+    var join_ref='/加入飯局資料/'+key;
+    db.ref(join_ref).once('value', function (snapshot) {
+        var data=snapshot.val();
+        var num=0
+        for(i in data){
+            console.log(data[i]);
+        }
+        
+    })
     console.log('你按的是' + key + '的More');
     var id = '#moreCompanion' + key;
     $(id).slideToggle();
+}
+
+function getimg(user, imgid) {
+    var img_id = imgid;
+    var storageRef = firebase.storage().ref();
+    var img_ref = 'user/' + user;
+    var pathReference = storageRef.child(img_ref);
+    pathReference.getDownloadURL().then(function (url) {
+        document.getElementById(img_id).src = url;
+    });
 }
